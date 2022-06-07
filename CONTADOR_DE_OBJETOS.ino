7b8effa194c1a8937299a8d9d907e456576868d3
Samuel Gomez <106413475+Dev-SamGomez@users.noreply.github.com>
2022-06-06 19:14:51 -0600

    Add files via upload

---
+++ CONTADOR_DE_OBJETOS.ino
@@ -0,0 +1,75 @@
+  #include<LiquidCrystal.h> 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+int pinoirent = 7;     
+int valorirent = 0; 
+int led=13;
+int content = 0; 
+int content1 = 0;   
+int total =0;
+void setup()
+{
+  Serial.begin(9600);            
+  pinMode(pinoirent, INPUT); ; 
+  lcd.begin(16,2);
+  pinMode (led,OUTPUT);
+  lcd.setCursor(0,0);            
+  lcd.print("ARTICULOS: "); 
+  
+}
+
+void loop()
+{
+   valorirent=digitalRead(pinoirent);
+  if(valorirent!=1)
+  {
+    content++; 
+    total++;
+     while(digitalRead(pinoirent)!=1) {
+        delay(100);     
+      }
+    lcd.setCursor(10,0); 
+    lcd.print(content); 
+    lcd.setCursor(0,1);
+    lcd.println("TOTAL:          ");
+    lcd.setCursor(7,1); 
+    lcd.print(total);
+  
+   if(content==4){
+          
+     lcd.clear();
+          lcd.setCursor(3,1); 
+          
+          lcd.println("LISTO           ");{
+          digitalWrite(led,1);
+          delay (700);
+          digitalWrite(led,0);
+          delay (700);
+          digitalWrite(led,1);
+          delay (700);
+          digitalWrite(led,0);
+          delay (700);
+          digitalWrite(led,1);
+          delay (700);
+          digitalWrite(led,0);
+          delay (700);
+          digitalWrite(led,1);
+          delay (700);
+          digitalWrite(led,0);
+          delay (700);
+   }
+          delay (1100);
+          
+          content=0;          
+          lcd.clear();
+          digitalWrite(led,0);
+          lcd.setCursor(0,0);            
+          lcd.print("ARTICULOS: ");
+          lcd.setCursor(0,1);
+          lcd.print("TOTAL:      "); 
+          lcd.setCursor(7,1);
+          lcd.print(total);
+          
+}
+  }
+}
+
